8e626c91b2be6f16306cb77f41af828bff1edba0
wnakano <nknvolpi@gmail.com>
2025-10-06 16:55:54 -0300

    feat: adding confirmation and cancelation nodes

--- apps/ai-service/src/ai/graph/nodes/conversational_qa/cancel_appointments.py
+++ apps/ai-service/src/ai/graph/nodes/conversational_qa/cancel_appointments.py
@@ -1,9 +1,11 @@
 from ...states.conversational_qa import QAState
 from ...types.conversational_qa import (
-    Routes, 
-    IntentType
+    Nodes,
+    DBAppointmentStatus
 )
+from ...services.conversational_qa import QueryORMService
 from utils import Logger
+
 
 logger = Logger(__name__)
 
@@ -11,10 +13,18 @@ logger = Logger(__name__)
 class CancelAppointmentNode:
     def __init__(
         self,
+        query_orm_service: QueryORMService
     ) -> None:
-        pass
+        self.query_orm_service = query_orm_service
     
     def __call__(self, state: QAState) -> QAState:
         logger.info("[NODE] CancelAppointmentNode")
-        return state
+        appointment_record = state.get("appointment_record")
+        appointment_id = appointment_record.appointment_id
+        _ = self.query_orm_service.update_appointment_status(
+            appointment_id=appointment_id,
+            new_status=DBAppointmentStatus.CONFIRMED
+        )
 
+        state["current_node"] = Nodes.CANCEL_APPOINTMENTS
+        return state
--- apps/ai-service/src/ai/graph/nodes/conversational_qa/confirm_appointments.py
+++ apps/ai-service/src/ai/graph/nodes/conversational_qa/confirm_appointments.py
@@ -1,21 +1,31 @@
 from ...states.conversational_qa import QAState
 from ...types.conversational_qa import (
-    Routes, 
-    IntentType
+	Nodes,
+	DBAppointmentStatus
 )
+from ...services.conversational_qa import QueryORMService
 from utils import Logger
 
 logger = Logger(__name__)
 
 
 class ConfirmAppointmentNode:
-    def __init__(
-        self,
-    ) -> None:
-        pass
-    
-    def __call__(self, state: QAState) -> QAState:
-        logger.info("[NODE] ConfirmAppointmentNode")
-        
-        return state
+	def __init__(
+		self,
+		query_orm_service: QueryORMService
+	) -> None:
+		self.query_orm_service = query_orm_service
+	
+	def __call__(self, state: QAState) -> QAState:
+		logger.info("[NODE] ConfirmAppointmentNode")
+		appointment_record = state.get("appointment_record")
+		appointment_id = appointment_record.appointment_id
+
+		_ = self.query_orm_service.update_appointment_status(
+			appointment_id=appointment_id,
+			new_status=DBAppointmentStatus.CONFIRMED
+		)
+
+		state["current_node"] = Nodes.CONFIRM_APPOINTMENTS
+		return state
 
