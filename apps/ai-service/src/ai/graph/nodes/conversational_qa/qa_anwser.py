a219acdb5ed957d68437708349078bcca93becd9
wnakano <nknvolpi@gmail.com>
2025-10-06 16:59:03 -0300

    feat: handling new intents (USER_INFORMATION, APPOINTMENT_INFORMATION)

--- apps/ai-service/src/ai/graph/nodes/conversational_qa/qa_anwser.py
+++ apps/ai-service/src/ai/graph/nodes/conversational_qa/qa_anwser.py
@@ -1,8 +1,8 @@
 from ...states.conversational_qa import QAState
 from ...types.conversational_qa import (
-    Nodes,
-    Routes, 
-    IntentType
+	Nodes,
+	Routes, 
+	IntentType
 )
 from ...services.conversational_qa import QAAnswerService
 from ...models.conversational_qa import QAAnswerModel
@@ -12,27 +12,27 @@ logger = Logger(__name__)
 
 
 class QAAnswerNode:
-    def __init__(
-        self,
-        qa_service: QAAnswerService
-    ) -> None:
-        self.qa_service = qa_service
-    
-    def __call__(self, state: QAState) -> QAState:
-        logger.info("[NODE] QAAnswerNode")
-        qa_result = self.qa_service.run(
-            state=state
-        )
-        system_message: QAAnswerModel = qa_result.system_answer
-        user_message = state.get("user_message", "")
+	def __init__(
+		self,
+		qa_service: QAAnswerService
+	) -> None:
+		self.qa_service = qa_service
+	
+	def __call__(self, state: QAState) -> QAState:
+		logger.info("[NODE] QAAnswerNode")
+		qa_result = self.qa_service.run(
+			state=state
+		)
+		system_message: QAAnswerModel = qa_result.qa_answer
+		user_message = state.get("user_message", "")
 
-        state["messages"] = state.get("messages", []) + [
-            {
-                "user_message": user_message,
-                "system_message": system_message
-            }
-        ]
-        state["current_node"] = Nodes.QA_ANSWER
+		state["messages"] = state.get("messages", []) + [
+			{
+				"user_message": user_message,
+				"system_message": system_message
+			}
+		]
+		state["current_node"] = Nodes.QA_ANSWER
 
-        return state
+		return state
 
