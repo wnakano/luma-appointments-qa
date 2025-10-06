d85f762108857307f35b3ccc07293051344394fa
wnakano <nknvolpi@gmail.com>
2025-10-06 16:57:20 -0300

    feat: adding action response node

--- apps/ai-service/src/ai/graph/nodes/conversational_qa/action_response.py
+++ apps/ai-service/src/ai/graph/nodes/conversational_qa/action_response.py
@@ -1,7 +1,10 @@
 from ...states.conversational_qa import QAState
 from ...types.conversational_qa import (
-    Routes, 
-    IntentType
+	Routes, 
+	Nodes,
+	IntentType,
+	ConfirmationIntent,
+	DBAppointmentStatus
 )
 from utils import Logger
 
@@ -9,13 +12,41 @@ logger = Logger(__name__)
 
 
 class ActionResponseNode:
-    def __init__(
-        self,
-    ) -> None:
-        pass
-    
-    def __call__(self, state: QAState) -> QAState:
-        logger.info("[NODE] ActionResponseNode")
+	def __init__(
+		self,
+	) -> None:
+		pass
+	
+	def __call__(self, state: QAState) -> QAState:
+		logger.info("[NODE] ActionResponseNode")
+		confirmation_result = state.get("confirmation_intent")
+		current_intent = state.get("current_intent")
+		messages = state.get("messages", [])
+		user_message = state.get("user_message")
 
-        return state
+		if current_intent == IntentType.LIST_APPOINTMENTS:
+			system_message = messages[-1]["system_message"]
+		else:
+			confirmation_intent = confirmation_result.intent
+			if confirmation_intent == ConfirmationIntent.CONFIRM:
+				system_message = "Your appointment has been "
+			elif confirmation_intent == ConfirmationIntent.REJECT:
+				system_message = "Your appointment has not been "
+			
+			if current_intent == IntentType.CANCEL_APPOINTMENT:
+				system_message += "canceled."
+			elif current_intent == IntentType.CONFIRM_APPOINTMENT:
+				system_message += "confirmed."
+
+		system_message += "\nIs there anything else I can do for you?"
+
+		state["messages"] = messages + [
+			{
+				"user_message": user_message,
+				"system_message": system_message
+			}
+		]
+		state["current_node"] = Nodes.ACTION_RESPONSE
+
+		return state
 
